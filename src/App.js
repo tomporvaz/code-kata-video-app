fd3ab4ccaa31609b468e34d5febe74d7d94ba767
tomporvaz <tomporvaz@gmail.com>
2020-02-04 17:00:52 -0500

    Completed challenge.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+const theBasics = { html : "https://www.youtube.com/embed/UB1O30fR-EE",
+css : "https://www.youtube.com/embed/yfoY53QXEnI",
+javascript : "https://www.youtube.com/embed/hdI2bqOjy3c" };
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    //clicked is not currently used, but it has been added in order to facilitate a styling updated based on which key is pressed.
+    this.state = {
+      "urlList": theBasics,
+      "currentIFrameURL": ""
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+ handleClick(linkName) {
+   console.log(linkName);
+   this.setState({
+     "currentIFrameURL": this.state.urlList[linkName]
+   })
+    
+  }
+
+  render() {
+    console.log(this.state.currentIFrameURL);
+    return(<div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <iframe src={this.state.currentIFrameURL} title="video-content"></iframe>
+        <div className="buttonContainer">
+          <button className="html" onClick={() => this.handleClick('html')}>html</button>
+          <button className="css" onClick={() => this.handleClick('css')}>css</button>
+          <button className="javascript" onClick={() => this.handleClick('javascript')}>javascript</button>
+
+        </div>
       </header>
-    </div>
-  );
+    </div>)
+  };
 }
 
 export default App;
