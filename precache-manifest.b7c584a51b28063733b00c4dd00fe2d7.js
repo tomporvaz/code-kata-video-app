f43f286646b2c943c2c1b2bb9fe3492cb00d96fd
tomporvaz <tomporvaz@gmail.com>
2020-02-05 19:33:31 -0500

    Updates

---
+++ precache-manifest.b7c584a51b28063733b00c4dd00fe2d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd0f3a0ddf4b4e34b0cd5cbc1122a362",
+    "url": "/code-kata-video-app/index.html"
+  },
+  {
+    "revision": "7350234db5c3945298cc",
+    "url": "/code-kata-video-app/static/css/main.0038dda7.chunk.css"
+  },
+  {
+    "revision": "11377c8cdfc35fab47da",
+    "url": "/code-kata-video-app/static/js/2.fcb1a2cb.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/code-kata-video-app/static/js/2.fcb1a2cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7350234db5c3945298cc",
+    "url": "/code-kata-video-app/static/js/main.d4aad7a1.chunk.js"
+  },
+  {
+    "revision": "ecd48badcb32d1a70518",
+    "url": "/code-kata-video-app/static/js/runtime-main.f6bb8caa.js"
+  }
+]);
